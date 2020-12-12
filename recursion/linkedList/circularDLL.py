214017c8d815561af996717c7fba9e3fcc74e6df
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2020-12-12 12:04:50 +0300

    Deleting node at specific location

--- recursion/linkedList/circularDLL.py
+++ recursion/linkedList/circularDLL.py
@@ -54,7 +54,77 @@ class CircularDLL:
                 node.prev = tempNode
                 node.next.prev = node
                 tempNode.next = node
+    
+    # traversing circular double linked list
+    def traversingCDLL(self):
+        if self.head is None:
+            print("List does not exist")
+        else:
+            tempNome = self.head
+            while tempNome:
+                print(tempNome.value)
+                if tempNome == self.tail:
+                    break
+                tempNome = tempNome.next
+    
+    # traversing backwords
+    def traversingCDLLBackword(self):
+        if self.head is None:
+            print("Linked does not exist")
+        else:
+            tempNode = self.tail 
+            while tempNode :
+                print(tempNode.value)
+                if tempNode == self.head:
+                    break
+                tempNode = tempNode.prev
+
+    def searchValue(self, value):
+        if self.head is None:
+            print("List does not exist")
+
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == value:
+                    return tempNode.value
+                if tempNode == self.tail:
+                    return "Node does not exist"
                 
+                tempNode = tempNode.next 
+                
+    def deletingNodeAtSpecificLocation(self, location):
+        if self.head is None:
+            print("List does not exist")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else: 
+                    self.head = self.head.next
+                    self.head.prev = self.tail
+                    self.tail.next = self.head
+            elif location == 1:
+                if self.head == self.tail:
+                    self.head.prev = None
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.tail = self.tail.prev
+                    self.tail.next = self.head
+                    self.head.prev = self.tail
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location -1:
+                    tempNode = tempNode.next
+                    index +=1
+                tempNode.next = tempNode.next.next
+                tempNode.next.prev = tempNode
 
 
                 
@@ -69,4 +139,16 @@ circularDLL.createLinkedList(3)
 circularDLL.insertNewNode(2,0)
 circularDLL.insertNewNode(4,1)
 circularDLL.insertNewNode(5,2)
+
+print([node.value for node in circularDLL])
+circularDLL.traversingCDLL()
+
+print([node.value for node in circularDLL])
+circularDLL.traversingCDLLBackword()
+
+print([node.value for node in circularDLL])
+print(circularDLL.searchValue(5))
+
+
+print(circularDLL.deletingNodeAtSpecificLocation(2))
 print([node.value for node in circularDLL])
